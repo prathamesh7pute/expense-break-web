3b04e4ae06e77db5d5d320aab3c2a511545ecae1
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2017-10-25 16:29:08 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Button from './Button';
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sheets: [{name: 'abc'}]
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://api.expensebreak.com/sheets')
+      .then(res => res.json())
+      .then(data => {
+        const sheets = data;
+        console.log(this.state);
+        console.log(sheets);
+
+        this.setState({ sheets });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,6 +37,11 @@ class App extends Component {
           Stay tune...
         </p>
         <Button name="Login" />
+        <ul>
+          {this.state.sheets.map((sheet, index) =>
+            <li key={sheet.index}>{sheet.name}</li>
+          )}
+        </ul>
       </div>
     );
   }
